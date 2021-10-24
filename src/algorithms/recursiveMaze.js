61982c5958673fefcfd673ffb26c31a24cec68b3
Alexander Bisztray <bisztraya@gmail.com>
2021-10-24 12:51:43 -0400

    Fixed Walls Obstructing Gaps
    Needed to change parity of their indices.

--- src/algorithms/recursiveMaze.js
+++ src/algorithms/recursiveMaze.js
@@ -1,21 +1,20 @@
 let walls = [];
 
 export const recursiveMaze = (grid, x1, x2, y1, y2) => {
-  // console.log(grid);
   let maze = getSubgrid(grid, x1 + 1, x2 - 1, y1 + 1, y2 - 1);
   divide(maze, 0, maze[0].length, 0, maze.length);
-
   return getWalls(maze);
 };
 
 const divide = (grid, x1, x2, y1, y2) => {
+  // console.log(grid);
   if (grid.length < 1) return;
 
   const width = grid[0].length;
   const height = grid.length;
 
   // base case
-  if (width < 2 || height < 2) return grid;
+  if (width < 2 || height < 2) return;
 
   // choose a direction in which to draw a wall
   const wallDirection =
@@ -23,56 +22,40 @@ const divide = (grid, x1, x2, y1, y2) => {
       ? "horizontal"
       : height < width
       ? "vertical"
-      : Math.floor(getRndInt(1, 2)) === 1
+      : Math.floor(Math.random() * 2) + 1 === 1
       ? "horizontal"
       : "vertical";
 
   // based on the direction, choose a location
   const wallPosition =
     wallDirection === "vertical"
-      ? Math.floor(width / 2)
-      : Math.floor(height / 2);
+      ? getRandomIntWithParity(1, width - 2, "odd")
+      : getRandomIntWithParity(1, height - 2, "odd");
 
   // choose a position in the wall to make a gap
   const gapPosition =
     wallDirection === "vertical"
-      ? getRndInt(0, height - 1)
-      : getRndInt(0, width - 1);
+      ? getRandomIntWithParity(0, height - 1, "even")
+      : getRandomIntWithParity(0, width - 1, "even");
 
   // make the wall
   wallDirection === "vertical"
     ? makeWallWithGap(getCol(grid, wallPosition), gapPosition)
     : makeWallWithGap(getRow(grid, wallPosition), gapPosition);
 
-  // // make recursive calls on resulting subgrids
+  // make recursive calls on resulting subgrids
   if (wallDirection === "vertical") {
     let leftHalf = getSubgrid(grid, x1, wallPosition, y1, y2);
     let rightHalf = getSubgrid(grid, wallPosition + 1, x2, y1, y2);
-    console.log(wallDirection, wallPosition, gapPosition);
     console.log([leftHalf, rightHalf]);
-    if (
-      leftHalf.length > 1 &&
-      rightHalf.length > 1 &&
-      leftHalf[0].length > 1 &&
-      rightHalf[0].length > 1
-    ) {
-      divide(leftHalf, 0, leftHalf[0].length, 0, leftHalf.length);
-      divide(rightHalf, 0, rightHalf[0].length, 0, rightHalf.length);
-    }
+    divide(leftHalf, 0, leftHalf[0].length, 0, leftHalf.length);
+    divide(rightHalf, 0, rightHalf[0].length, 0, rightHalf.length);
   } else {
     let topHalf = getSubgrid(grid, x1, x2, y1, wallPosition);
     let bottomHalf = getSubgrid(grid, x1, x2, wallPosition + 1, y2);
-    console.log(wallDirection, wallPosition, gapPosition);
     console.log([topHalf, bottomHalf]);
-    if (
-      topHalf.length > 1 &&
-      bottomHalf.length > 1 &&
-      topHalf[0].length > 1 &&
-      bottomHalf[0].length > 1
-    ) {
-      divide(topHalf, 0, topHalf[0].length, 0, topHalf.length);
-      divide(bottomHalf, 0, bottomHalf.length, 0, bottomHalf.length);
-    }
+    divide(topHalf, 0, topHalf[0].length, 0, topHalf.length);
+    divide(bottomHalf, 0, bottomHalf.length, 0, bottomHalf.length);
   }
 };
 
@@ -99,8 +82,11 @@ const makeWallWithGap = (gridSegment, gapPosition) => {
   walls.push(gridSegment);
 };
 
-const getRndInt = (min, max) => {
-  return Math.floor(Math.random() * (max - min + 1)) + min;
+const getRandomIntWithParity = (min, max, parity) => {
+  let result = Math.floor(Math.random() * (max - min + 1)) + min;
+  if (result % 2 === 0 && parity === "even") return result;
+  if (result % 2 !== 0 && parity === "odd") return result;
+  return result === max ? result - 1 : result + 1;
 };
 
 const getWalls = (grid) => {
